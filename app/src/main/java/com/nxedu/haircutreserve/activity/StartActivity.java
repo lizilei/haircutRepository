d0dfb7548c1eabdae558d7f7179cda2c57bc3cb7
lzl <lizilei_warms@163.com>
2017-03-22 22:12:11 +0800

    第三次提交

--- app/src/main/java/com/nxedu/haircutreserve/activity/StartActivity.java
+++ app/src/main/java/com/nxedu/haircutreserve/activity/StartActivity.java
@@ -1,12 +1,17 @@
 package com.nxedu.haircutreserve.activity;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.nxedu.haircutreserve.R;
 
 import org.kymjs.kjframe.ui.BindView;
+import org.kymjs.kjframe.ui.ViewInject;
 
 /**
  * @author dupeng
@@ -15,19 +20,29 @@ import org.kymjs.kjframe.ui.BindView;
  */
 public class StartActivity extends BaseActivity {
 
-    @BindView(id = R.id.tv, click = true)
-    private TextView tv;
+    private TimeCount time;
+    @BindView(id = R.id.title_middle_tv)
+    private TextView mTvTitle;
+    @BindView(id = R.id.edt_login_activity_user_phone)
+    private EditText mEdtUserPhone;
+    @BindView(id = R.id.edt_login_activity_user_verification_code)
+    private EditText mEdtUserVerificationCode;
+    @BindView(id = R.id.btn_login_activity_user_verification_code ,click = true)
+    private Button mBtnVerificationCode;
+    @BindView(id = R.id.btn_login_activity_login ,click = true)
+    private Button mBtnUserLogin;
 
     @Override
     public void setRootView() {
         super.setRootView();
         setContentView(R.layout.activity_start);
+        time = new TimeCount(60000, 1000);
     }
 
     @Override
     public void initData() {
         super.initData();
-
+        mTvTitle.setText("理发店管理系统");
     }
 
     @Override
@@ -40,11 +55,56 @@ public class StartActivity extends BaseActivity {
     public void widgetClick(View v) {
         super.widgetClick(v);
         switch (v.getId()) {
-            case R.id.tv:
+            case R.id.btn_login_activity_login:
                 startActivity(new Intent(StartActivity.this, MainActivity.class));
+                finish();
                 break;
+
+            case R.id.btn_login_activity_user_verification_code:
+                time.start();
+                break;
+
             default:
+                break;
         }
     }
+
+    class TimeCount extends CountDownTimer {
+        public TimeCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onFinish() {// 计时完毕
+            mBtnVerificationCode.setText("再次获取");
+            mBtnVerificationCode.setClickable(true);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {// 计时过程
+            mBtnVerificationCode.setClickable(false);//防止重复点击
+            mBtnVerificationCode.setText(millisUntilFinished / 1000 + "秒");
+        }
+    }
+
+    private long mExitTime;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            //=====双键退出==
+            if ((System.currentTimeMillis() - mExitTime) > 2000) {
+                Object mHelperUtils;
+                ViewInject.toast("再按一次退出程序");
+                mExitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
+
 }
 

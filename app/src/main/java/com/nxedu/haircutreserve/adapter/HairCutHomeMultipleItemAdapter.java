8e8d10c8d7ac20e54adb5c5b79513cdb6b83cf3e
lzl <lizilei_warms@163.com>
2017-03-23 22:41:07 +0800

    修改了部分代码

--- app/src/main/java/com/nxedu/haircutreserve/adapter/HairCutHomeMultipleItemAdapter.java
+++ app/src/main/java/com/nxedu/haircutreserve/adapter/HairCutHomeMultipleItemAdapter.java
@@ -41,7 +41,7 @@ public class HairCutHomeMultipleItemAdapter extends RecyclerView.Adapter<Recycle
         this.context = context;
 //        this.results = results;
         options = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.mipmap.ic_launcher) //设置图片在下载期间显示的图片
+        .showImageOnLoading(R.mipmap.ic_launcher) //设置图片在下载期间显示的图片
                 .showImageForEmptyUri(R.mipmap.ic_launcher)//设置图片Uri为空或是错误的时候显示的图片
                 .showImageOnFail(R.mipmap.ic_launcher)  //设置图片加载/解码过程中错误时候显示的图片
                 .cacheInMemory(true)//设置下载的图片是否缓存在内存中
